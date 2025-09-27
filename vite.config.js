f09320312255552a43554a900f612a9ba65ea225
Jihwan-bit <yoonjihwan@gmail.com>
2025-09-27 15:24:24 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 // vite.config.js (루트)
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/moyuum_recommender/',   // ← 저장소명과 일치
   build: { outDir: 'dist' }       // ← dist 로 빌드
 })
